f9793bcdc0815be76980de6cf6725d01cb484b07
PJayChen <n26021539@mail.ncku.edu.tw>
2015-08-13 20:56:19 +0800

    first build

---
+++ firmware/data_temp_sens_rgb.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx_bt_sk configuration
+CND_ARTIFACT_DIR_pic32mx_bt_sk=dist/pic32mx_bt_sk/production
+CND_ARTIFACT_NAME_pic32mx_bt_sk=data_temp_sens_rgb.X.production.hex
+CND_ARTIFACT_PATH_pic32mx_bt_sk=dist/pic32mx_bt_sk/production/data_temp_sens_rgb.X.production.hex
+CND_PACKAGE_DIR_pic32mx_bt_sk=${CND_DISTDIR}/pic32mx_bt_sk/package
+CND_PACKAGE_NAME_pic32mx_bt_sk=datatempsensrgb.x.tar
+CND_PACKAGE_PATH_pic32mx_bt_sk=${CND_DISTDIR}/pic32mx_bt_sk/package/datatempsensrgb.x.tar
